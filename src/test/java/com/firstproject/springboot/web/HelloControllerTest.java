7079b5050b3f2e2de9937df6e0280109e9410a2a
Hyebin-Park <foreverinthesun.daum.net>
2021-01-06 21:22:24 +0900

    Repository 작성

--- src/test/java/com/firstproject/springboot/web/HelloControllerTest.java
+++ src/test/java/com/firstproject/springboot/web/HelloControllerTest.java
@@ -1,4 +1,5 @@
-package com.firstproject.springboot.web;
+package com.firstproject.springboot.web.dto;
+import com.firstproject.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
