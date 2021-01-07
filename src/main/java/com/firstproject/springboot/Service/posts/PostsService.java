9d54aea8da2e13e0151695037e9fd8bac2f26831
Hyebin-Park <foreverinthesun.daum.net>
2021-01-07 22:13:09 +0900

    PostService 클래스 생성

---
+++ src/main/java/com/firstproject/springboot/Service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.firstproject.springboot.Service.posts;
+
+import com.firstproject.springboot.domain.posts.PostsRepository;
+import com.firstproject.springboot.web.Dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
