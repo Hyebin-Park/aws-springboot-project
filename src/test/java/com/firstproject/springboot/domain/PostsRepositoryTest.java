c435b63db0e20ac11889072912a40130ebb0d4fe
Hyebin-Park <foreverinthesun@daum.net>
2021-01-11 20:10:16 +0900

    Auditing 테스트

--- src/test/java/com/firstproject/springboot/domain/PostsRepositoryTest.java
+++ src/test/java/com/firstproject/springboot/domain/PostsRepositoryTest.java
@@ -1,5 +1,8 @@
-package com.firstproject.springboot.domain.posts;
+package com.firstproject.springboot.domain;
 
+import com.firstproject.springboot.domain.posts.Posts;
+import com.firstproject.springboot.domain.posts.PostsRepository;
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,5 +47,27 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+
+        // given
+        LocalDateTime now = LocalDateTime.of(2021, 1, 1, 12, 1, 30);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("HB")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        System.out.println(">>>createDate : " + posts.getCreatedDate() + "modifiedDate : " + posts.getModifiedTime());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedTime()).isAfter(now);
+
     }
 }
