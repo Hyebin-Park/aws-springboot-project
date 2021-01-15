e7c955851fc63a0c7da018c6a0aec022df1ec621
Hyebin-Park <foreverinthesun@daum.net>
2021-01-15 17:58:29 +0900

    수정 화면 구현

--- src/main/java/com/firstproject/springboot/web/IndexController.java
+++ src/main/java/com/firstproject/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.firstproject.springboot.web;
 
 import com.firstproject.springboot.Service.posts.PostsService;
+import com.firstproject.springboot.web.Dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -12,15 +14,25 @@ public class IndexController {
 
     private final PostsService postsService;
 
+    // 메인(홈)화면
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
+    // 글 작성 & 저장 화면
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    // 수정 화면
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 
 }
