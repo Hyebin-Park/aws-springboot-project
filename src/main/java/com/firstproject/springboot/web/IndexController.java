aadf8a9bc19926a1a50cbc33efdd45dfe1385bd8
Hyebin-Park <foreverinthesun@daum.net>
2021-01-17 18:56:55 +0900

    어노테이션 기반으로 개선

--- src/main/java/com/firstproject/springboot/web/IndexController.java
+++ src/main/java/com/firstproject/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.firstproject.springboot.web;
 
 import com.firstproject.springboot.Service.posts.PostsService;
+import com.firstproject.springboot.config.auth.LoginUser;
 import com.firstproject.springboot.config.auth.dto.SessionUser;
 import com.firstproject.springboot.web.Dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,23 +10,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     // 메인(홈)화면
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
-            model.addAttribute("userName", user.getName());
+            model.addAttribute("userName", user.getName())
         }
         return "index";
     }
