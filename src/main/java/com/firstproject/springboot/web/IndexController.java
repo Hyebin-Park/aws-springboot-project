7beb8eb6e4e88ddacc0a1043e2ded81656810692
Hyebin-Park <foreverinthesun@daum.net>
2021-01-17 12:45:32 +0900

    로그인 테스트

--- src/main/java/com/firstproject/springboot/web/IndexController.java
+++ src/main/java/com/firstproject/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.firstproject.springboot.web;
 
 import com.firstproject.springboot.Service.posts.PostsService;
+import com.firstproject.springboot.config.auth.dto.SessionUser;
 import com.firstproject.springboot.web.Dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,16 +9,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     // 메인(홈)화면
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
